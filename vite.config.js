4473e57f5f94378cd82b07109e23462fff4aa415
jairo51067 <jairocardenas05@gmail.com>
2026-04-20 13:47:09 -0400

    Mejorar carga erronea

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,26 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@app': path.resolve(__dirname, './src/app'),
-      '@domain': path.resolve(__dirname, './src/domain'),
-      '@application': path.resolve(__dirname, './src/application'),
-      '@infrastructure': path.resolve(__dirname, './src/infrastructure'),
-      '@presentation': path.resolve(__dirname, './src/presentation'),
-    }
+      "@": path.resolve(__dirname, "./src"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@config": path.resolve(__dirname, "./src/config"),
+      "@domain": path.resolve(__dirname, "./src/domain"),
+      "@application": path.resolve(__dirname, "./src/application"),
+      "@infrastructure": path.resolve(__dirname, "./src/infrastructure"),
+      "@presentation": path.resolve(__dirname, "./src/presentation"),
+    },
   },
   server: {
     port: 3000,
-    open: true
+    open: true,
   },
   build: {
-    outDir: 'dist'
-  }
+    outDir: "dist",
+  },
 });
